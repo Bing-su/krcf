63fc4c846e45c659dde0af76ec2121df30c2686b
Dowon <ks2515@naver.com>
2025-08-04 15:00:32 +0900

    ci: separate release and test workflows

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,15 @@
+import platform
+
 import nox
+
+if platform.machine() == "ARM64" and platform.system() == "Windows":
+    python = ["3.13", "3.13t"]
+else:
+    python = ["3.9", "3.13", "3.13t", "pypy3.11"]
 
 
 @nox.session(
-    python=["3.9", "3.13", "3.13t", "pypy3.11"],
+    python=python,
     venv_backend="uv",
 )
 def test(session: nox.Session) -> None:
