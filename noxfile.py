aadacb3a9445815daf6a0452cffc93de6ce04dac
Dowon <ks2515@naver.com>
2025-08-04 15:14:19 +0900

    ci: remove windows arm test

--- noxfile.py
+++ noxfile.py
@@ -1,16 +1,12 @@
-import platform
-
 import nox
 
-if platform.machine() == "ARM64" and platform.system() == "Windows":
-    python = ["3.13", "3.13t"]
-else:
-    python = ["3.9", "3.13", "3.13t", "pypy3.11"]
+python = ["3.9", "3.13", "3.13t", "pypy3.11"]
+venv_backend = "uv"
 
 
 @nox.session(
     python=python,
-    venv_backend="uv",
+    venv_backend=venv_backend,
 )
 def test(session: nox.Session) -> None:
     session.install(".[test]")
