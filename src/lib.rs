688e9ca3ff482dbc0d341dbc561d6d813990dc66
Dowon <ks2515@naver.com>
2025-08-05 17:31:25 +0900

    feat: cargo workspace

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 use pyo3::prelude::*;
 mod pyrcf;
-mod rcf;
 
-/// A Python module implemented in Rust.
 #[pymodule]
 fn krcf(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
